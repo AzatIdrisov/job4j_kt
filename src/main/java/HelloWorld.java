555a997254ecd7680fb63c5693bec96525569489
idrisov.a <idrisov.a@spectrumdata.ru>
2023-09-19 09:40:54 +0500

    1. Первая программа. [#188590]

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+import ru.job4j.base.HelloWorldKt;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Java");
+        HelloWorldKt.main(args);
+    }
+}
