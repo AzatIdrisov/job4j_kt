f4bdff07949084640dd06534d4a7bcbd8d1c0f1a
idrisov.a <idrisov.a@spectrumdata.ru>
2023-11-10 11:00:53 +0500

    small fix

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,6 @@
-import ru.job4j.base.HelloWorldKt;
-
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello from Java");
-        HelloWorldKt.main(args);
     }
 }
